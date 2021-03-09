538babe7623a0f12abf661a601279ba2a09832a4
gabor <gabor.marosi@hotmail.com>
2021-03-09 23:21:43 +0100

    dynamodb tabla es 2 lambda def

--- web_shell_mg/web_shell_mg_stack.py
+++ web_shell_mg/web_shell_mg_stack.py
@@ -1,10 +1,10 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (
+    core as cdk,
+    aws_lambda,
+    aws_dynamodb,
+    aws_events,
+    aws_events_targets,
+)
 
 
 class WebShellMgStack(cdk.Stack):
@@ -12,4 +12,42 @@ class WebShellMgStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        web_shell_table = aws_dynamodb.Table(
+            self, "web_shell_table",
+            partition_key=aws_dynamodb.Attribute(
+                name="id",
+                type=aws_dynamodb.AttributeType.STRING
+            )
+        )
+
+        # create producer lambda function
+        producer_lambda = aws_lambda.Function(self, "producer_lambda_function",
+                                              runtime=aws_lambda.Runtime.PYTHON_3_6,
+                                              handler="lambda_function.lambda_handler",
+                                              code=aws_lambda.Code.asset("./lambda/producer"))
+
+        producer_lambda.add_environment("TABLE_NAME", web_shell_table.table_name)
+
+        # grant permission to lambda to write to demo table
+        web_shell_table.grant_write_data(producer_lambda)
+
+        # create consumer lambda function
+        consumer_lambda = aws_lambda.Function(self, "consumer_lambda_function",
+                                              runtime=aws_lambda.Runtime.PYTHON_3_6,
+                                              handler="lambda_function.lambda_handler",
+                                              code=aws_lambda.Code.asset("./lambda/consumer"))
+
+        consumer_lambda.add_environment("TABLE_NAME", web_shell_table.table_name)
+
+        # grant permission to lambda to read from demo table
+        web_shell_table.grant_read_data(consumer_lambda)
+
+        # create a Cloudwatch Event rule
+        one_minute_rule = aws_events.Rule(
+            self, "one_minute_rule",
+            schedule=aws_events.Schedule.rate(cdk.Duration.minutes(1)),
+        )
+
+        # Add target to Cloudwatch Event
+        one_minute_rule.add_target(aws_events_targets.LambdaFunction(producer_lambda))
+        one_minute_rule.add_target(aws_events_targets.LambdaFunction(consumer_lambda))
